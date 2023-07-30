7ce2da8e0201c40957c8a11c40d4ac0166b4b761
Bryan Jacobs <b@q3q.us>
2023-07-30 20:39:26 +1000

    Delete obsolete tests

--- src/test/java/us/q3q/fido2/UnitTesting.java
+++ src/test/java/us/q3q/fido2/UnitTesting.java
@@ -8,8 +8,6 @@ import javacard.framework.ISO7816;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import us.q3q.fido2.FIDO2Applet;
-import us.q3q.fido2.FIDOConstants;
 
 import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
@@ -17,7 +15,6 @@ import javax.smartcardio.ResponseAPDU;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 /**
  * Example (only example) unit tests with jcardsim
@@ -150,178 +147,7 @@ public class UnitTesting {
 
         byte[] respWithoutStatus = new byte[resp.length-2];
         System.arraycopy(resp, 0, respWithoutStatus, 0, resp.length-2);
-        assertEquals("U2F_V2", new String(respWithoutStatus));
+        assertEquals("FIDO_2_0", new String(respWithoutStatus));
     }
 
-    @Test
-    public void testVersionInfo() {
-        ResponseAPDU response = sendCTAP(0x04);
-
-        assertEquals(ISO7816.SW_NO_ERROR, (short) response.getSW());
-
-        byte[] data = response.getData();
-        assertEquals(49, data.length);
-        assertEquals(FIDOConstants.CTAP2_OK, data[0]);
-    }
-
-    @Test
-    public void testMakeCredential() {
-        ResponseAPDU response = sendCTAP(
-       0x01,                                      // authenticatorMakeCredential command
-             0xa5,                                      // map(5)
-   0x01,                                   // unsigned(1) - clientDataHash
-   0x58, 0x20,                                // bytes(32)
-      0x68, 0x71, 0x34, 0x96, 0x82, 0x22, 0xec, 0x17, 0x20, 0x2e, 0x42, 0x50, 0x5f, 0x8e, 0xd2, 0xb1,  // h’687134968222ec17202e42505f8ed2b16ae22f16bb05b88c25db9e602645f141'
-      0x6a, 0xe2, 0x2f, 0x16, 0xbb, 0x05, 0xb8, 0x8c, 0x25, 0xdb, 0x9e, 0x60, 0x26, 0x45, 0xf1, 0x41,  //
-   0x02,                                   // unsigned(2) - rp
-   0xa2,                                   // map(2)
-      0x62,                                // text(2)
-         0x69, 0x64,                           // "id"
-      0x6b,                                // text(11)
-         0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d,         // "example.com"
-      0x64,                                // text(4)
-         0x6e, 0x61, 0x6d, 0x65,                       // "name"
-      0x64,                                // text(4)
-         0x41, 0x63, 0x6d, 0x65,                       // "Acme"
-   0x03,                                   // unsigned(3) - user
-   0xa4,                                   // map(4)
-      0x62,                                // text(2)
-         0x69, 0x64,                           // "id"
-      0x58, 0x20,                             // bytes(32)
-         0x30, 0x82, 0x01, 0x93, 0x30, 0x82, 0x01, 0x38, 0xa0, 0x03, 0x02, 0x01, 0x02,     // userid
-         0x30, 0x82, 0x01, 0x93, 0x30, 0x82, 0x01, 0x38, 0xa0, 0x03, 0x02, 0x01, 0x02,     // ...
-         0x30, 0x82, 0x01, 0x93, 0x30, 0x82,                   // ...
-      0x64,                                // text(4)
-         0x69, 0x63, 0x6f, 0x6e,                       // "icon"
-      0x78, 0x2b,                             // text(43)
-         0x68, 0x74, 0x74, 0x70, 0x73, 0x3a, 0x2f, 0x2f, 0x70, 0x69, 0x63, 0x73, 0x2e, 0x65, 0x78, // "https://pics.example.com/00/p/aBjjjpqPb.png"
-         0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x30, 0x30, 0x2f, 0x70, 0x2f, //
-         0x61, 0x42, 0x6a, 0x6a, 0x6a, 0x70, 0x71, 0x50, 0x62, 0x2e, 0x70, 0x6e, 0x67,     //
-      0x64,                                // text(4)
-         0x6e, 0x61, 0x6d, 0x65,                       // "name"
-      0x76,                                // text(22)
-         0x6a, 0x6f, 0x68, 0x6e, 0x70, 0x73, 0x6d, 0x69, 0x74, 0x68, 0x40, 0x65, 0x78, 0x61, 0x6d, // "johnpsmith@example.com"
-         0x70, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d,                  // ...
-      0x6b,                                 // text(11)
-         0x64, 0x69, 0x73, 0x70, 0x6c, 0x61, 0x79, 0x4e, 0x61, 0x6d, 0x65,          // "displayName"
-      0x6d,                                 // text(13)
-         0x4a, 0x6f, 0x68, 0x6e, 0x20, 0x50, 0x2e, 0x20, 0x53, 0x6d, 0x69, 0x74, 0x68,      // "John P. Smith"
-   0x04,                                    // unsigned(4) - pubKeyCredParams
-   0x82,                                    // array(2)
-      0xa2,                                 // map(2)
-         0x63,                              // text(3)
-            0x61, 0x6c, 0x67,                       // "alg"
-         0x26,                              // -7 (ES256)
-         0x64,                              // text(4)
-            0x74, 0x79, 0x70, 0x65,                     // "type"
-         0x6a,                              // text(10)
-            0x70, 0x75, 0x62, 0x6C, 0x69, 0x63, 0x2D, 0x6B, 0x65, 0x79,         // "public-key"
-      0xa2,                                 // map(2)
-         0x63,                              // text(3)
-            0x61, 0x6c, 0x67,                       // "alg"
-         0x39, 0x01, 0x00,                          // -257 (RS256)
-         0x64,                              // text(4)
-            0x74, 0x79, 0x70, 0x65,                     // "type"
-         0x6a,                              // text(10)
-            0x70, 0x75, 0x62, 0x6C, 0x69, 0x63, 0x2D, 0x6B, 0x65, 0x79,         // "public-key"
-   0x07,                                    // unsigned(7) - options
-   0xa1,                                    // map(1)
-      0x62,                                 // text(2)
-         0x72, 0x6b,                            // "rk"
-      0xf5                                 // primitive(21)
-        );
-
-        assertEquals(ISO7816.SW_NO_ERROR, (short) response.getSW());
-
-        byte[] data = response.getData();
-        assertNotEquals(0, data.length);
-        assertEquals(FIDOConstants.CTAP2_OK, data[0]);
-    }
-
-    @Test
-    public void testGetAssertion_invalid() {
-        ResponseAPDU response = sendCTAP(
-                0x02,                                      // authenticatorGetAssertion command
-                0xA4,                                      // map(4)
-                0x01,                                   // unsigned(1)
-                0x6b,                                   // text(11)
-        0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d,            // "example.com"
-        0x02,                                   // unsigned(2)
-        0x58, 0x20,                                // bytes(32)
-        0x68, 0x71, 0x34, 0x96, 0x82, 0x22, 0xec, 0x17, 0x20, 0x2e, 0x42, 0x50, 0x5f, 0x8e, 0xd2, 0xb1,   // clientDataHash
-        0x6a, 0xe2, 0x2f, 0x16, 0xbb, 0x05, 0xb8, 0x8c, 0x25, 0xdb, 0x9e, 0x60, 0x26, 0x45, 0xf1, 0x41,   // ...
-        0x03,                                    // unsigned(3)
-        0x82,                                    // array(2)
-        0xa2,                                 // map(2)
-        0x62,                              // text(2)
-        0x69, 0x64,                         // "id"
-        0x58, 0x40,                           // bytes(64)
-        0xf2, 0x20, 0x06, 0xde, 0x4f, 0x90, 0x5a, 0xf6, 0x8a, 0x43, 0x94, 0x2f, 0x02,  // credential ID
-        0x4f, 0x2a, 0x5e, 0xce, 0x60, 0x3d, 0x9c, 0x6d, 0x4b, 0x3d, 0xf8, 0xbe, 0x08,   // ...
-        0xed, 0x01, 0xfc, 0x44, 0x26, 0x46, 0xd0, 0x34, 0x85, 0x8a, 0xc7, 0x5b, 0xed,   // ...
-        0x3f, 0xd5, 0x80, 0xbf, 0x98, 0x08, 0xd9, 0x4f, 0xcb, 0xee, 0x82, 0xb9, 0xb2,   // ...
-        0xef, 0x66, 0x77, 0xaf, 0x0a, 0xdc, 0xc3, 0x58, 0x52, 0xea, 0x6b, 0x9e,     // ...
-        0x64,                              // text(4)
-        0x74, 0x79, 0x70, 0x65,                     // "type"
-        0x6a,                              // text(10)
-        0x70, 0x75, 0x62, 0x6C, 0x69, 0x63, 0x2D, 0x6B, 0x65, 0x79,         // "public-key"
-        0xa2,                                 // map(2)
-        0x62,                              // text(2)
-        0x69, 0x64,                         // "id"
-        0x58, 0x32,                           // bytes(50)
-        0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03,   // credential ID
-        0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03,   // ...
-        0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03,   // ...
-        0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03,  // ...
-        0x64,                              // text(4)
-        0x74, 0x79, 0x70, 0x65,                     // "type"
-        0x6a,                              // text(10)
-        0x70, 0x75, 0x62, 0x6C, 0x69, 0x63, 0x2D, 0x6B, 0x65, 0x79,         // "public-key"
-        0x05,                                    // unsigned(5)
-        0xa1,                                    // map(1)
-        0x62,                                 // text(2)
-        0x75, 0x76,                            // "uv"
-        0xf5                                 // true
-        );
-
-        assertEquals(ISO7816.SW_NO_ERROR, (short) response.getSW());
-
-        byte[] data = response.getData();
-        assertEquals(1, data.length);
-        assertEquals(FIDOConstants.CTAP2_ERR_NO_CREDENTIALS, data[0]);
-    }
-
-    @Test
-    public void testSystemDCryptEnroll() {
-        ResponseAPDU r0 = sendCTAP(
-        "01A5015820000000000000000000000000000000000000000000000000000000000000000002A262696475696F2E73797374656D642E63727970747365747570646E616D6570456E6372797074656420566F6C756D6503A3626964582435643930333061632D373133302D343963302D393431382D373863336364333362613165646E616D65782435643930333061632D373133302D343963302D393431382D3738633363643333626131656B646973706C61794E616D65781C2F686F6D652F627261756A61632F73632F70726574656E646469736B0481A263616C672664747970656A7075626C69632D6B657906A16B686D61632D736563726574F500"
-        );
-        assertEquals(ISO7816.SW_NO_ERROR, (short) r0.getSW());
-        byte[] data = r0.getData();
-        assertNotEquals(0, data.length);
-        assertEquals(FIDOConstants.CTAP2_OK, data[0]);
-
-        ResponseAPDU r1 = sendCTAP(
-        "06A20101020263"
-        );
-        assertEquals(ISO7816.SW_NO_ERROR, (short) r1.getSW());
-        data = r1.getData();
-        assertNotEquals(0, data.length);
-        assertEquals(FIDOConstants.CTAP2_OK, data[0]);
-
-        ResponseAPDU r2 = sendCTAP(
-            "02A50175696F2E73797374656D642E6372797074736574757002582000000000000000000000000000000000000000000000000000000000000000000381A26269645860CCF7130EE121D29ED8AEC5DEB3A6761F560DDF91C59F4ADF3749BD286C6B27A6B650193F72C858F1D27EA4DE60B85D018A773966E035327618610986B7392930A59D1E85ED99FC43BC34C0334A710DC7313565A6BD9354460D2F59B6C7CC4D7C64747970656A7075626C69632D6B657904A16B686D61632D736563726574A301A501020338182001215820320CCE78B2F352F133B0D658C58BF6F66EAE2C484346A254AAA9340407E9E378225820765D5EA42BED8A4E00F522AC6BEC3615FEE22F55729923BF81C7DEAB52A372EE0258206A1C6D9CCC4804A88C009DF802A4DB21C77B3220789D65DDCC47D50743A2761503507483E3D7D969D03505BAE3DE96CBF95F05A1627570F5B3"
-        );
-        assertEquals(ISO7816.SW_NO_ERROR, (short) r2.getSW());
-        data = r2.getData();
-        assertNotEquals(0, data.length);
-        assertEquals(FIDOConstants.CTAP2_OK, data[0]);
-    }
-
-    public void testHWSecurity() {
-        // 80100000C701A6015820B38AD58E8EB3E54BABBB66218BA14C16B8057C5F26F9D0AC8002DFEF4677E49002A2626964766669646F2D6C6F67696E2E6578616D706C652E636F6D646E616D65781A4649444F2D4578616D706C652052656C79696E6720506172747903A36269645030B21834C24F1511CE4A9279E79B047F646E616D656874657374757365726B646973706C61794E616D656874657374757365720481A263616C672664747970656A7075626C69632D6B6579085024593DBB753EC6F81B005F0FA45D6BF9090100
-        ResponseAPDU r0 = sendCTAP(
-                "A6015820B38AD58E8EB3E54BABBB66218BA14C16B8057C5F26F9D0AC8002DFEF4677E49002A2626964766669646F2D6C6F67696E2E6578616D706C652E636F6D646E616D65781A4649444F2D4578616D706C652052656C79696E6720506172747903A36269645030B21834C24F1511CE4A9279E79B047F646E616D656874657374757365726B646973706C61794E616D656874657374757365720481A263616C672664747970656A7075626C69632D6B6579085024593DBB753EC6F81B005F0FA45D6BF90901"
-        );
-    }
 }
