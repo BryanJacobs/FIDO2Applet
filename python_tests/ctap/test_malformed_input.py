bd76745a4302d3fa9b03e96e4f14d69cadcd82e5
Bryan Jacobs <b@q3q.us>
2023-08-01 20:40:44 +1000

    Test for non-integer pubkey algs

--- python_tests/ctap/test_malformed_input.py
+++ python_tests/ctap/test_malformed_input.py
@@ -10,3 +10,12 @@ class CTAPMalformedInputTestCase(CTAPTestCase):
         with self.assertRaises(CtapError) as e:
             self.ctap2.make_credential(**self.basic_makecred_params)
         self.assertEqual(CtapError.ERR.MISSING_PARAMETER, e.exception.code)
+
+    def test_rejects_non_integer_alg_in_array(self):
+        self.basic_makecred_params['key_params'].append({
+            "type": "public-key",
+            "alg": "foo"
+        })
+        with self.assertRaises(CtapError) as e:
+            self.ctap2.make_credential(**self.basic_makecred_params)
+        self.assertEqual(CtapError.ERR.CBOR_UNEXPECTED_TYPE, e.exception.code)
